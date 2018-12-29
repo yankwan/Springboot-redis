a19f057410949bd3702135ea55b4933b84928e49
liuyanjun <yanjliu@126.com>
2018-12-29 10:12:05 +0800

    init jedis-demo

---
+++ jedisdemo/src/main/java/redisjedisdemo/jedisdemo/JedisdemoApplication.java
@@ -0,0 +1,14 @@
+package redisjedisdemo.jedisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedisdemoApplication.class, args);
+    }
+
+}
+
