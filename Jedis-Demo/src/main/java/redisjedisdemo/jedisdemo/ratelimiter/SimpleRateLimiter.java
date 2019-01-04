c91d6357885f60aaee537a517dfdac64f2cd81cd
liuyanjun <yanjliu@126.com>
2019-01-04 18:01:33 +0800

    ratelimit

---
+++ Jedis-Demo/src/main/java/redisjedisdemo/jedisdemo/ratelimiter/SimpleRateLimiter.java
@@ -0,0 +1,41 @@
+package redisjedisdemo.jedisdemo.ratelimiter;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+
+import java.io.IOException;
+
+public class SimpleRateLimiter {
+    private Jedis jedis;
+
+    public SimpleRateLimiter(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) throws IOException {
+        String key = String.format("hist:%s:%s", userId, actionKey);
+        long nowTs = System.currentTimeMillis();
+        Pipeline pipe = jedis.pipelined();
+        pipe.multi();
+        pipe.zadd(key, nowTs, "" + nowTs);
+        pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+        Response<Long> count = pipe.zcard(key);
+        pipe.expire(key, period + 1);
+        pipe.exec();
+        pipe.close();
+        return count.get() <= maxCount;
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis();
+        SimpleRateLimiter limiter = new SimpleRateLimiter(jedis);
+        try {
+            for(int i = 0; i < 20; i++) {
+                System.out.println(limiter.isActionAllowed("user", "reply", 60, 5));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
